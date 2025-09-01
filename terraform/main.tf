40321bc999bc5f90866a2f1f0342fcae5012656b
So Ezeh <117824198+SokEarth@users.noreply.github.com>
2025-09-01 17:35:40 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,32 @@
 terraform {
-  required_version = ">= 1.13.1"
-
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = ">= 4.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.46.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
+    }
+
     kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = ">= 2.0"
-    }
-    helm = {
-      source = "hashicorp/helm"
-      version = ">= 2.0"
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.16.1"
     }
   }
+
+  required_version = "~> 1.3"
 }
 
 provider "aws" {
