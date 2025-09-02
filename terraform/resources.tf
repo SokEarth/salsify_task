e84ccfa69241049c9a83f2ac36a1c0bb040d6fb0
SokEarth <sooezeh@gmail.com>
2025-09-02 09:17:02 +0100

    Deploy EKS

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -101,7 +101,7 @@ module "eks" {
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
   eks_managed_node_groups = {
-   example = {
+   task_node = {
      instance_types = ["t3.medium"]
      min_size       = 1
      max_size       = 3
